16a1bd23b781aad069146d8d6b6c6e735e3822fc
Aurenisu <suprisebaby15@gmail.com>
2026-03-31 15:43:05 +0300

    Update ana.py

--- ana.py
+++ ana.py
@@ -23,7 +23,7 @@ YTDL_OPTIONS = {
 
 FFMPEG_OPTIONS = {
     'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-    'options': '-vn'
+    'options': '-vn -loglevel quiet'
 }
 
 # ytdl tanımlamasını buraya sabitledik
